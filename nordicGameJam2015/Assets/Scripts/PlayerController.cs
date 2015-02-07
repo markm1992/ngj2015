85fff1467c366177e842cfe9bc239ccd22edf620
markm1992 <markm1992@yahoo.com>
2015-02-07 19:21:06 +0100

    Lots of changes
    2 hours worth. Plays cleanly. Interactions and story in scenes

--- nordicGameJam2015/Assets/Scripts/PlayerController.cs
+++ nordicGameJam2015/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour {
     float tsInput;
 	public storyLine story;
 	public Animator animator;
+	bool canInput = true;
 
 	// Use this for initialization
 	void Start () {
@@ -18,32 +19,47 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        msInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(0, 0, msInput * walkSpeed * Time.deltaTime));
-        tsInput = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(tsInput * turnSpeed * Time.deltaTime, 0, 0));
-		if(Mathf.Abs(msInput ) > 0)
-			animator.SetFloat("speed", Mathf.Abs(msInput ));
-		else
-			animator.SetFloat("speed", Mathf.Abs(tsInput));
-        if (Input.GetAxis("Mouse X") > .1 || Input.GetAxis("Mouse X") < -.1)
-        {
-            rsInput = Input.GetAxis("Mouse X");
-            transform.Rotate(new Vector3(0, rsInput * rotateSpeed * Time.deltaTime, 0));
-        }
+		if (canInput) {
+			msInput = Input.GetAxis ("Vertical");
+			transform.Translate (new Vector3 (0, 0, msInput * walkSpeed * Time.deltaTime));
+			tsInput = Input.GetAxis ("Horizontal");
+			transform.Translate (new Vector3 (tsInput * turnSpeed * Time.deltaTime, 0, 0));
+			if (Mathf.Abs (msInput) > 0)
+				animator.SetFloat ("speed", Mathf.Abs (msInput));
+			else
+				animator.SetFloat ("speed", Mathf.Abs (tsInput));
+			if (Input.GetAxis ("Mouse X") > .1 || Input.GetAxis ("Mouse X") < -.1) {
+				rsInput = Input.GetAxis ("Mouse X");
+				transform.Rotate (new Vector3 (0, rsInput * rotateSpeed * Time.deltaTime, 0));
+			}
+		}
 	}
 
 	void OnTriggerStay(Collider other)
 	{
 		if (other.tag == "Story" && Input.GetKeyDown (KeyCode.E)) {
+			animator.SetFloat("speed", 0);
+			canInput = false;
+			StartCoroutine("interact");
 			other.gameObject.SendMessage ("trigger", SendMessageOptions.DontRequireReceiver);
 		} else if (other.tag == "Finish") {
+			canInput = false;
+			animator.SetFloat("speed", 0);
 			win ();
 		} else if (other.tag == "Obstacle") {
+			canInput = false;
+			animator.SetFloat("speed", 0);
 			story.bearAtk();
 		}
 	}
-
+	
+	IEnumerator interact()
+	{
+		//play interact animation
+		yield return new WaitForSeconds (1.5f);
+		canInput = true;
+		yield return true;
+	}
 	void win()
 	{
 		story.win();
